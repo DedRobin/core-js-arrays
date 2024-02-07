31fc5c566529df747198770196e6c90788466c69
DedRobin <pavelrabekin@gmail.com>
2024-02-07 14:17:05 +0300

    feat: task 'generateOdds' is solved

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -345,8 +345,15 @@ function createChunks(/* arr, chunkSize */) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  let counter = 1;
+  const array = new Array(len).fill(0);
+  const oddArray = array.map((item, i) => {
+    const num = item + i + counter;
+    counter += 1;
+    return num;
+  });
+  return oddArray;
 }
 
 /**
